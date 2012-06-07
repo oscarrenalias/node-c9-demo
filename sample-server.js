11f0833751ab4fa92bd4077385f9e93b40bab4bb
Oscar Renalias <oscar.renalias@accenture.com>
2012-06-07 11:18:47 +0300

    Removed an unnecessary file

--- sample-server.js
+++
@@ -1,7 +0,0 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
-}).listen(process.env.PORT, '0.0.0.0');
-
-console.log('Server running at port ' + process.env.C9_PORT);
